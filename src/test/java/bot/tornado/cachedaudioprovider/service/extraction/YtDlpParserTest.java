7c05bab88ab691dc62c283b5bd91dee4dfc2b82e
staubtornado <jbkirche@gmail.com>
2025-09-26 16:41:08 +0200

    Update YtDlpParserTest.java

--- src/test/java/bot/tornado/cachedaudioprovider/service/extraction/YtDlpParserTest.java
+++ src/test/java/bot/tornado/cachedaudioprovider/service/extraction/YtDlpParserTest.java
@@ -4,6 +4,7 @@ import bot.tornado.cachedaudioprovider.dto.YtDlpEntry;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class YtDlpParserTest {
 
     @Autowired
